4ca71b47dc41c9abec022e7e564c1f135a1f9153
Keaun Moughari <mougharik@gmail.com>
2019-10-12 17:24:26 -0400

    Add part1 to repo

---
+++ part1/part1.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{ 
+    printf("This is a test\n");
+    printf("This is a test\n");
+    printf("This is a test\n");
+    printf("This is a test\n");
+    printf("This is a test\n");
+    printf("This is a test\n");
+    return 0;
+}
